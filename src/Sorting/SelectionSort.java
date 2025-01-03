7e52303877d1ccc1481ef82eeb7fe50ac826a657
codebyte <sujeet.jog@gmail.com>
2025-01-03 18:31:47 +0530

    first commit, creating the directory
    Selection Sort

---
+++ src/Sorting/SelectionSort.java
@@ -0,0 +1,31 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    public static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void sort(int a[]) {
+        int idx = 0;
+        for(int i = 0; i < a.length; i++) {
+           idx = i;
+           for(int j = i+1; j < a.length; j++) {
+                if(a[j] < a[idx]) {
+                    idx = j;
+                }
+           }
+           swap(a, idx, i);
+        }
+    }
+
+    public static void main(String args[]) {
+        int a[] = {10, 4, 6, 78, 2, 67, 8};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
