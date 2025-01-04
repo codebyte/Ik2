bc97718b2750e045a1e716c822c076e579181bb3
codebyte <sujeet.jog@gmail.com>
2025-01-04 10:51:16 +0530

    first commit, creating the directory
    Selection Sort

---
+++ src/Sorting/InsertionSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+
+    public static void sort(int a[]) {
+        for(int i = 0; i < a.length; i++) {
+            int val = a[i];
+            int j = i;
+            for(; (j > 0) && (val < a[j-1]); j--) {
+                a[j] = a[j-1];
+            }
+            a[j] = val;
+        }
+   }
+
+
+
+
+    public static void main(String args[]) {
+        int a[] = {23, 5, 7, 89, 9};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+
+}
