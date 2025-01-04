0a132f7aead8152eaa19826cb30b3ed3ef8424c1
codebyte <sujeet.jog@gmail.com>
2025-01-04 09:21:31 +0530

    first commit, creating the directory
    Selection Sort

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,29 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void swap(int a[], int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void sort(int a[]) {
+        for(int i = 0; i < a.length; i++) {
+           for(int j = a.length-1; j > i; j--) {
+                if(a[j] < a[j-1]) {
+                    swap(a, j, j-1);
+                }
+           }
+        }
+    }
+
+
+    public static void main(String args[]) {
+        int a[] = {10 , 56, 7, 8, 34, 6};
+        sort(a);
+        System.out.println(Arrays.toString(a));
+    }
+}
