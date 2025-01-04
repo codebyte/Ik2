11dd1fa69fb0ef10fb9cb417a33b558a84a494b8
codebyte <sujeet.jog@gmail.com>
2025-01-04 12:13:59 +0530

    insertion sort

---
+++ src/Sorting/MergeSort.java
@@ -0,0 +1,53 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void mergeSort(int a[], int start, int end) {
+
+        if(start >=end) {
+            return;
+        }
+
+        int mid = start + (end -start)/2;
+
+        mergeSort(a, start, mid);
+        mergeSort(a, mid+1, end);
+
+        int i = start;
+        int j = mid + 1;
+
+        int k = 0;
+        int aux[] = new int[end - start + 1];
+
+        while((i <= mid) && (j <= end)) {
+            if(a[i] <= a[j]) {
+                aux[k++] = a[i++];
+            } else {
+               aux[k++] = a[j++];
+            }
+        }
+
+        while(i <= mid) {
+            aux[k++] = a[i++];
+        }
+
+        while(j <= end) {
+            aux[k++] = a[j++];
+        }
+
+        for(k = 0, i = start; (k < aux.length) && (i <= end); k++, i++) {
+           a[i] = aux[k];
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int a[] = { 213, 5, 67, 68, 8, 9, 2, 4};
+        mergeSort(a, 0, a.length-1);
+        System.out.println(Arrays.toString(a));
+
+
+    }
+}
