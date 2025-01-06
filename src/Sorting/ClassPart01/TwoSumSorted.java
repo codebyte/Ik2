851e69e6d5cf982c7fcae5066dcc90f87326ce24
codebyte <sujeet.jog@gmail.com>
2025-01-06 10:11:55 +0530

    Two sum sorted

--- src/Sorting/ClassPart01/TwoSumSorted.java
+++ src/Sorting/ClassPart01/TwoSumSorted.java
@@ -26,19 +26,23 @@ Output:
 [-1, -1]
 */
 package Sorting.ClassPart01;
-
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 
-public class TwoSum {
-    public static boolean twoSum(int a[], int target) {
+public class TwoSumSorted {
+    public static ArrayList<Integer> twoSum(int a[], int target) {
+
+        ArrayList<Integer> result = new ArrayList<>();
         qSort(a, 0, a.length);
         int i = 0;
         int j = a.length-1;
         while(i < j) {
             int sum = a[i] + a[j];
             if(sum == target) {
-                return true;
+                result.add(i);
+                result.add(j);
+                return result;
             }
             if(sum > target) {
                 j--;
@@ -46,7 +50,9 @@ public class TwoSum {
                i++;
             }
         }
-        return false;
+        result.add(-1);
+        result.add(-1);
+        return result;
     }
 
     public static int randomNumber(int start, int end) {
@@ -84,10 +90,26 @@ public class TwoSum {
         qSort(a, pivot+1, end);
     }
 
+    static ArrayList<Integer> two_sum(ArrayList<Integer> numbers, Integer target) {
+
+        int a[] = new int[numbers.size()];
+        for(int i = 0; i < a.length; i++) {
+            a[i] = numbers.get(i);
+        }
+        return twoSum(a, target);
+    }
+
     public static void main(String args[]) {
         int a[] = {10, 34, 456, 5, 7, 78, 0};
         qSort(a, 0, a.length);
+
+        ArrayList<Integer>  list = new ArrayList<>();
+        for(int i = 0; i < a.length; i++) {
+            list.add(a[i]);
+        }
+        two_sum(list, 88);
+
         System.out.println(Arrays.toString(a));
-        System.out.println(twoSum(a, 87));
+        System.out.println(twoSum(a, 88));
     }
 }
